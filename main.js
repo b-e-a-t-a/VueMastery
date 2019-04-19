2644c335a47de8f20664dd9f380d582cfb30a12a
Beata Karolczak <karolczak.beata@gmail.com>
2019-04-19 18:30:42 +0200

    Attribute binding, conditional rendering, list rendering, event handling, class & style binding

---
+++ main.js
@@ -0,0 +1,39 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    product: "Socks",
+    description: "A pair of warm, fuzzy socks",
+    image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+    link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
+    altText: "A pair of socks",
+    inventory: 1,
+    inStock: false,
+    onSale: true,
+    details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: 'green',
+        variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg'
+      },
+      {
+        variantId: 2235,
+        variantColor: 'blue',
+        variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg'
+      }
+    ],
+    sizes: ["35","36","37","38"],
+    cart: 0
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1
+    },
+    removeFromCart() {
+      this.cart -= 1
+    },
+    updateProduct(variantImage) {
+      this.image = variantImage
+    }
+  }
+})
