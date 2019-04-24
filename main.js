6c697f358b8e1f0756b9b10542cf9bb9f4f040e1
Beata Karolczak <karolczak.beata@gmail.com>
2019-04-24 22:41:53 +0200

    Computed properties

--- main.js
+++ main.js
@@ -1,25 +1,27 @@
 var app = new Vue({
   el: '#app',
   data: {
+    brand: "Vue Mastery",
     product: "Socks",
     description: "A pair of warm, fuzzy socks",
-    image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+    selectedVariant: 0,
     link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
     altText: "A pair of socks",
     inventory: 1,
-    inStock: false,
     onSale: true,
     details: ['80% cotton', '20% polyester', 'Gender-neutral'],
     variants: [
       {
         variantId: 2234,
         variantColor: 'green',
-        variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg'
+        variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+        variantQuantity: 10
       },
       {
         variantId: 2235,
         variantColor: 'blue',
-        variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg'
+        variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg',
+        variantQuatnity: 0
       }
     ],
     sizes: ["35","36","37","38"],
@@ -32,8 +34,25 @@ var app = new Vue({
     removeFromCart() {
       this.cart -= 1
     },
-    updateProduct(variantImage) {
-      this.image = variantImage
+    updateProduct(index) {
+      this.selectedVariant = index
+    }
+  },
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity
+    },
+    sale() {
+      if (this.onSale) {
+        return this.brand + ' ' + this.product + ' are on sale!'
+      }
+       return this.brand + ' ' + this.product + ' are not on sale!'
     }
   }
 })
